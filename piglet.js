9f925ae3cae4469bce9769c9e8065e04c6e5c367
OpenShift guest <xyz@apip.de>
2013-07-12 12:03:52 -0400

    add file transfer to piglet server

--- piglet.js
+++ piglet.js
@@ -1,30 +1,46 @@
 var net = require('net');
 var spawn = require('child_process').spawn
+var fs = require('fs');
+
 
 var HOST = process.env.IP;
 var PORT = 17000;
 
-var shellcomand = 'echo';
+var shellcommzand = 'echo';
 
 function command(regexp, callback){
     
-var extract    = spawn(shellcommand, [regexp]);
 
-    extract.stdout.on('data', function (data) {
-        
-      console.log('stdout: ' + data);
+var event_command    = spawn('./analyze_events.sh');
+
+    event_command.stdout.on('data', function (data) {
       
-      /* Here return callback data in the final version */
-      //callback(data);
-      callback('Hello World');
-      
+       var trace_command = spawn('./analyze_traces.sh');
+       
+       trace_command.stdout.on('data', function (data) {
+            
+            spawn('rm',['sequence.txt']);
+           
+            var hdsf_pull    = spawn('hadoop',['fs','-pull /process/sequence/part-00000 sequence.txt']);
+            hdsf_pull.stdout.on('data', function (data) {
+                fs.readFile('./local_test_client/process.log', function (err, data) {
+                if (err) throw err;
+                    callback(data);
+                });
+            });
+            fs.readFile('./local_test_client/process.log', function (err, data) {
+                if (err) throw err;
+                    callback(data);
+            });
+            
+        });
     });
     
-    extract.stderr.on('data', function (data) {
+    event_command.stderr.on('data', function (data) {
       console.log('stderr: ' + data);
     });
     
-    extract.on('exit', function (code) {
+    event_command.on('exit', function (code) {
       console.log('child process exited with code ' + code);
     });
 }
